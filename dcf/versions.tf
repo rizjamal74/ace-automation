0e7c4a6d2652935d7774037c3968de3b655a5671
John Smoker <jsmoker@aviatrix.com>
2025-06-16 19:46:30 -0700

    formatting

--- dcf/versions.tf
+++ dcf/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
     workspaces {
       name = "ace-automation-dcf"
