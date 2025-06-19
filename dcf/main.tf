58a96ecc4d77b8174e6ab8ad66b72e87bb143f90
Rizwan Jamal <68023044+rizjamal74@users.noreply.github.com>
2025-06-19 02:04:09 -0400

    dcf port 80 new changes

--- dcf/main.tf
+++ dcf/main.tf
@@ -58,9 +58,9 @@ resource "aviatrix_distributed_firewalling_policy_list" "default" {
   }
 
   policies {
-    name     = "allow-rfc1918"
+    name     = "allow-rfc1918-a"
     action   = "PERMIT"
-    priority = 1002
+    priority = 1005
     protocol = "TCP"
     logging  = true
     watch    = false
